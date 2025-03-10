45293dd7e0c295f25a5e4c9382aff1fa1580165b
Odunsegun <israelodunaiya@gmail.com>
2025-03-09 20:03:11 -0400

    3 body problem

---
+++ worksheet3_2/evolve.py
@@ -0,0 +1,29 @@
+from nbody import Particle, System
+
+s = System.read("earthsun.txt")
+
+dt = 0.01
+tf = 100
+ti = 0
+
+f = open("data.txt", "w")
+
+t = ti
+while t < tf:
+	
+	# drift
+	s.update_positions(0.5 * dt)
+	
+	# kick
+	s.calc_accels()
+	s.update_velocities(dt)
+	
+	# drift
+	s.update_positions(0.5 * dt)
+	
+	t += dt
+	s.time = t
+	
+	f.write(f"{t} {s.particles[0].x()} {s.particles[0].y()} {s.particles[1].x()} {s.particles[1].y()}\n")
+	
+f.close()
